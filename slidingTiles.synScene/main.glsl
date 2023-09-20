13c0a415f8e49b1d94ff834d7e0695a99e20aa0d
Harry <harrytsaban@gmail.com>
2023-09-20 17:38:28 +0100

    Tweak sliding tile scene

--- slidingTiles.synScene/main.glsl
+++ slidingTiles.synScene/main.glsl
@@ -6,11 +6,11 @@ float avg(float x, float y) { return (x + y) / 2; }
 float hypoteneuse(float x, float y) { return sqrt(sq(x) + sq(y)); }
 float parabola(float x) { return sq(4.0 * x * (1.0 - x)); }
 vec4 temporal_morph(vec4 a, vec4 b) {
-  float red = (sq(cos(TIME / 12)) * max(a.x, b.x)) +
+  float red = (sq(cos(TIME / 120)) * max(a.x, b.x)) +
               (sq(sin(TIME / 12)) * avg(a.x, b.x));
-  float blue = (sq(sin(TIME / 11)) * max(a.y, b.y)) +
+  float blue = (sq(sin(TIME / 110)) * max(a.y, b.y)) +
                (sq(cos(TIME / 11)) * avg(a.y, b.y));
-  float green = (sq(sin(TIME / 9)) * hypoteneuse(a.z, b.z)) +
+  float green = (sq(sin(TIME / 90)) * hypoteneuse(a.z, b.z)) +
                 (sq(cos(TIME / 9)) * avg(a.z, b.z));
   return vec4(red, blue, green, hypoteneuse(a.a, b.a));
 }
@@ -18,11 +18,12 @@ vec4 temporal_morph(vec4 a, vec4 b) {
 vec4 renderMain(void) {
   float red, green, blue;
   vec2 position = _uv;
-  vec2 offset = vec2(sin(TIME / 9) * x_trans_amt, cos(TIME / 13) * y_trans_amt);
+  vec2 offset = vec2(sin(TIME / 90) * x_trans_amt, cos(TIME / 130) * y_trans_amt);
   position += offset;
   float tileCount =
-      1 + sin(syn_BassTime / 5) + sin(syn_HighTime) + sin(syn_MidTime / 3);
-  vec2 tile = fract(position * tileCount);
+      1 + sin(syn_BassTime / 50) + sin(syn_HighTime/10) + sin(syn_MidTime / 30);
+  vec2 tile = fract(_rotate(position * tileCount, 2*PI*_ncos(TIME/17)));
+ 
   red = parabola(tile.x * tile.x) * parabola(tile.y * tile.y);
   green = parabola(tile.x) + parabola(tile.y);
   blue = tan(2 * PI * (tile.x * tile.x - tile.y * tile.y));
